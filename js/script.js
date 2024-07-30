c51b42945f899441747fbc4ccd27b71963dd572e
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-07-30 15:50:03 +0700

    update

--- js/script.js
+++ js/script.js
@@ -11,6 +11,64 @@ function removeModal() {
     $('.modal').remove();
 }
 
+function sendForm(data) {
+    console.log(data);
+    $.ajax({
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSc2bPWCILkgnbQ_vFQXpFIM6CSPZ3okInbNmjz_GwARt2fPbQ/formResponse",
+        data: {
+          "entry.897534436": data.name,
+          "entry.1439971969": data.address,
+          "entry.1862797708": data.phone,
+          "entry.18376172": data.email,
+          "entry.1433351178": data.product,
+        },
+        type: "POST",
+        dataType: "json",
+        statusCode: {
+          0: function () {
+          },
+          200: function () {
+          },
+        },
+      });
+}
+
+function submitModal () {
+    var nameM = $('#name-m');
+    var addressM = $('#address-m');
+    var phoneM = $('#phone-m');
+    var emailM = $('#email-m');
+    var productM = $('#product-m');
+    var submitB = $('#submit-m');
+    if(!nameM.val() || !addressM.val() || !phoneM.val() || !emailM.val() || !productM.val()){
+        alert('Vui lòng điền đầy đủ thông tin trước khi gửi');
+    } else {
+        var data = {
+            name: nameM.val(),
+            address: addressM.val(),
+            phone: phoneM.val(),
+            email: emailM.val(),
+            product: productM.val()
+        };
+        sendForm(data);
+        submitB.prop('disabled', true);
+        submitB.attr('style', 'filter: grayscale(1);');
+        submitB.val('Đang gửi thông tin');
+        setTimeout(function(){
+            alert('Thành công');
+            nameM.val("");
+            addressM.val("");
+            phoneM.val("");
+            emailM.val("");
+            productM.val("");
+            submitB.removeAttr('disabled');
+            submitB.removeAttr('style');
+            submitB.val('Đăng ký');
+            removeModal();
+        }, 2000)
+    }
+}
+
 $(document).ready(function () {
     // Cache selectors
     var lastId,
@@ -86,22 +144,22 @@ $(document).ready(function () {
                             </div>
                             <div class="modal__form">
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Họ và tên">
+                                    <input class="modal__input" id="name-m" type="text" placeholder="Họ và tên">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Địa chỉ">
+                                    <input class="modal__input" id="address-m" type="text" placeholder="Địa chỉ">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Số điện thoại">
+                                    <input class="modal__input" id="phone-m" type="text" placeholder="Số điện thoại">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Email">
+                                    <input class="modal__input" id="email-m" type="text" placeholder="Email">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Sản phẩm muốn đăng ký vay mua nhà">
+                                    <input class="modal__input" id="product-m" type="text" placeholder="Sản phẩm muốn đăng ký vay mua nhà">
                                 </div>
                                 <div class="modal__submit">
-                                    <input class="button-1" type="button" value="Đăng ký">
+                                    <input class="button-1" id="submit-m" onclick="submitModal()" type="button" value="Đăng ký">
                                 </div>
                             </div>
                         </div>
@@ -121,22 +179,22 @@ $(document).ready(function () {
                             </div>
                             <div class="modal__form">
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Họ và tên">
+                                    <input class="modal__input" id="name-m" type="text" placeholder="Họ và tên">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Địa chỉ">
+                                    <input class="modal__input" id="address-m" type="text" placeholder="Địa chỉ">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Số điện thoại">
+                                    <input class="modal__input" id="phone-m" type="text" placeholder="Số điện thoại">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Email">
+                                    <input class="modal__input" id="email-m" type="text" placeholder="Email">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Sản phẩm muốn đăng ký vay kinh doanh">
+                                    <input class="modal__input" id="product-m" type="text" placeholder="Sản phẩm muốn đăng ký vay kinh doanh">
                                 </div>
                                 <div class="modal__submit">
-                                    <input class="button-1" type="button" value="Đăng ký">
+                                    <input class="button-1" id="submit-m" onclick="submitModal()" type="button" value="Đăng ký">
                                 </div>
                             </div>
                         </div>
@@ -155,23 +213,23 @@ $(document).ready(function () {
                                 <img src="images/title-p3.jpg" alt="">
                             </div>
                             <div class="modal__form">
-                                <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Họ và tên">
+                                 <div class="form-group">
+                                    <input class="modal__input" id="name-m" type="text" placeholder="Họ và tên">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Địa chỉ">
+                                    <input class="modal__input" id="address-m" type="text" placeholder="Địa chỉ">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Số điện thoại">
+                                    <input class="modal__input" id="phone-m" type="text" placeholder="Số điện thoại">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Email">
+                                    <input class="modal__input" id="email-m" type="text" placeholder="Email">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Sản phẩm thẻ tín dụng muốn đăng ký">
+                                    <input class="modal__input" id="product-m" type="text" placeholder="Sản phẩm thẻ tín dụng muốn đăng ký">
                                 </div>
-                                <div class="modal__submit">
-                                    <input class="button-1" type="button" value="Đăng ký">
+                               <div class="modal__submit">
+                                    <input class="button-1" id="submit-m" onclick="submitModal()" type="button" value="Đăng ký">
                                 </div>
                             </div>
                         </div>
@@ -191,22 +249,22 @@ $(document).ready(function () {
                             </div>
                             <div class="modal__form">
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Họ và tên">
+                                    <input class="modal__input" id="name-m" type="text" placeholder="Họ và tên">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Địa chỉ">
+                                    <input class="modal__input" id="address-m" type="text" placeholder="Địa chỉ">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Số điện thoại">
+                                    <input class="modal__input" id="phone-m" type="text" placeholder="Số điện thoại">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Email">
+                                    <input class="modal__input" id="email-m" type="text" placeholder="Email">
                                 </div>
                                 <div class="form-group">
-                                    <input class="modal__input" type="text" placeholder="Sản phẩm bảo hiểm muốn đăng ký">
+                                    <input class="modal__input" id="product-m" type="text" placeholder="Sản phẩm bảo hiểm muốn đăng ký">
                                 </div>
                                 <div class="modal__submit">
-                                    <input class="button-1" type="button" value="Đăng ký">
+                                    <input class="button-1" id="submit-m" onclick="submitModal()" type="button" value="Đăng ký">
                                 </div>
                             </div>
                         </div>
@@ -232,6 +290,7 @@ $(document).ready(function () {
     $('.next__screen4').click(function() {
         $('.screen4 .container4').css('display', 'none');
         $('.screen4__slide').css('display', 'block');
+        $('.screen4').addClass('isActive');
     });
 
     $('.next__screen5').click(function() {
@@ -241,4 +300,39 @@ $(document).ready(function () {
 
     var swiper4 = new Swiper(".mySwiper4", {});
     var swiper5 = new Swiper(".mySwiper5", {});
-})
+
+    //form bottom
+    $('#submit-b').on('click', function() {
+        var nameB = $('#name-b');
+        var addressB = $('#address-b');
+        var phoneB = $('#phone-b');
+        var emailB = $('#email-b');
+        var productB = $('#product-b');
+        if(!nameB.val() || !addressB.val() || !phoneB.val() || !emailB.val() || !productB.val()){
+            alert('Vui lòng điền đầy đủ thông tin trước khi gửi');
+        } else {
+            var data = {
+                name: nameB.val(),
+                address: addressB.val(),
+                phone: phoneB.val(),
+                email: emailB.val(),
+                product: productB.val()
+            };
+            sendForm(data);
+            $('#submit-b').prop('disabled', true);
+            $('#submit-b').attr('style', 'filter: grayscale(1);');
+            $('#submit-b').val('Đang gửi thông tin');
+            setTimeout(function(){
+                alert('Thành công');
+                nameB.val("");
+                addressB.val("");
+                phoneB.val("");
+                emailB.val("");
+                productB.val("");
+                $('#submit-b').removeAttr('disabled');
+                $('#submit-b').removeAttr('style');
+                $('#submit-b').val('Đăng ký');
+            }, 2000)
+        }
+    })
+});
